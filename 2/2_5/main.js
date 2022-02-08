443672323739f6386cdd19960660a077b8563237
Aleksandra <akrysinska@emyad.com>
2022-02-08 14:21:13 +0100

    Zadanie 1 - Modal

--- 2/2_5/main.js
+++ 2/2_5/main.js
@@ -2,6 +2,18 @@ new Vue({
     el: "#app",
     data: {
         buttonStyle: 'display:block;',
-        modalStyle: 'display:none'
+        modalStyle: 'display:none;',
+        header: 'Witaj świecie',
+        content: 'Hej, <a href="#">Thomas</a>!'
+    },
+    methods: {
+        openModal: function() {
+            this.buttonStyle = this.modalStyle;
+            this.modalStyle = 'display:flex;';
+        },
+        hideModal: function() {
+            this.buttonStyle = 'display: block;';
+            this.modalStyle = 'display: none;'
+        }
     }
 });
