b29a9783fd780415e7a8769f14ebae06e31d6733
Aleksandra <akrysinska@emyad.com>
2022-02-11 15:49:27 +0100

    cd

--- 5/5_8/5_8/src/App.vue
+++ 5/5_8/5_8/src/App.vue
@@ -1,33 +1,37 @@
 <template>
   <div id="app">
-    
+    <TheAddChampionPanel />
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'app',
-    data: () => ({
-      champions:[
-        {
-          name: 'Lux',
-          pseudo: 'the Light',
-          hp: 120,
-          mp: 890
-        },
-        {
-          name: 'Macho',
-          pseudo: 'the Strong',
-          hp: 690,
-          mp: 80
-        }
-      ]
-    })
-  }
+import TheAddChampionPanel from "./components/TheAddChampionPanel";
+export default {
+  name: "app",
+  components: {
+    TheAddChampionPanel,
+  },
+  data: () => ({
+    champions: [
+      {
+        name: "Lux",
+        pseudo: "the Light",
+        hp: 120,
+        mp: 890,
+      },
+      {
+        name: "Macho",
+        pseudo: "the Strong",
+        hp: 690,
+        mp: 80,
+      },
+    ],
+  }),
+};
 </script>
 
 <style>
-  #app{
-    padding:20px;
-  }
+#app {
+  padding: 20px;
+}
 </style>
---
+++ 5/5_8/5_8/src/components/AboutChampionPanel.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="panel panel-primary">
+    <div class="panel-heading">
+      <h4 class="panel-title">Dodawanie Championa</h4>
+    </div>
+    <div class="panel-body">
+      <div class="input-group margin-bottom">
+        <span class="input-group-addon">Imie postaci</span>
+        <input
+          type="text"
+          class="form-control"
+          v-model="name"
+          placeholder="Imię"
+        />
+      </div>
+      <div class="input-group margin-bottom">
+        <span class="input-group-addon">Przydomek postaci</span>
+        <input
+          type="text"
+          class="form-control"
+          v-model="pseudo"
+          placeholder="Przydomek"
+        />
+      </div>
+      <div class="row margin-bottom">
+        <div class="col-md-6">
+          <div class="input-group">
+            <span class="input-group-addon">Max HP</span>
+            <input
+              v-model="hp"
+              type="number"
+              class="form-control"
+              placeholder="Maksymalna ilość HP"
+            />
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="input-group">
+            <span class="input-group-addon">Przydomek postaci</span>
+            <input
+              v-model="mp"
+              type="number"
+              class="form-control"
+              placeholder="Maksymalna ilość HP"
+            />
+          </div>
+        </div>
+      </div>
+      <button type="button" class="btn btn-danger" @click="reset">
+        Resetuj
+      </button>
+      &nbsp;
+      <button type="button" class="btn btn-success" @click="add">Dodaj</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutChampionPanel",
+  data: () => ({
+    name: "",
+    pseudo: "",
+    hp: null,
+    mp: null,
+  }),
+  methods: {
+    reset() {
+      this.name = "";
+      this.pseudo = "";
+      this.hp = null;
+      this.mp = null;
+    },
+    add() {
+      this.$emit("add", {
+        name: this.name,
+        pseudo: this.pseudo,
+        hp: this.hp,
+        mp: this.mp,
+      });
+      this.reset();
+    },
+  },
+};
+</script>
+
+<style scoped>
+.margin-bottom {
+  margin-bottom: 12px;
+}
+</style>
